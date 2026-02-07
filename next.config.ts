04306fd1c4af82e0bb23c5bd86e4dd81e4fe5d46
Aerley Lopez <Lopezkeller65@gmail.com>
2026-02-07 16:03:12 -0600

    habilitando dominos externo - CORS fix

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    externalDir: true,
+  },
+  allowedDevOrigins: ["*"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "pub-266f56f2e24d4d3b8e8abdb612029f2f.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "fonts.googleapis.com",
       },
     ],
   },
