614e52607cf489644cb2cdde993e8de207d38ef0
Aerley Lopez <Lopezkeller65@gmail.com>
2026-04-16 11:54:00 -0600

    eliminando imports innecesarios

--- app/dashboard/maps/page.tsx
+++ app/dashboard/maps/page.tsx
@@ -2,8 +2,6 @@
 
 import { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-import TrackerMap from "@/components/TrackerMap";
 
 const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
@@ -151,6 +149,6 @@ export default function Page() {
   }, []);
 
   return (
-    <TrackerMap />
+    null
   );
 }
--- app/dashboard/visitas/maps/page.tsx
+++ app/dashboard/visitas/maps/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import TrackerMap from "@/components/TrackerMap";
-
 export default function Page() {
   return (
     <div className="h-[92vh] relative">
-      <TrackerMap />
     </div>
   );
 }
